c36ec773a9ac89b4b0c606a23e11f96dde86a94d
liuwei2 <liuwei2@digisky.com>
2018-09-26 18:00:51 +0800

    学习go fmt

--- src/stacker.go
+++ src/stacker.go
@@ -1,19 +1,61 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"goinaction/src/statistics"
-	"time"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
-func main()  {
-	statistics.Start()
-	count, _ := fmt.Println("服务器启动成功")
-	fmt.Println(count)
-	go log("Hello World!")
-	time.Sleep(time.Second)
+func main() {
+	TestSimplifyWitespace()
 }
 
-func log(str string)  {
+func log(str string) {
 	fmt.Println(str)
+}
+
+func str1() {
+	line := "ra ssd \u2028 va"
+	i := strings.IndexFunc(line, unicode.IsSpace)
+	firstWord := line[:i]
+	j := strings.LastIndexFunc(line, unicode.IsSpace)
+	_, size := utf8.DecodeRuneInString(line[j:])
+	lastWord := line[j+size:]
+	fmt.Println(firstWord, lastWord)
+
+	var str string
+	str = "VV"
+	fmt.Println("Name: ", str)
+}
+
+func SimplifyWitespace(s string) string {
+	var buffer bytes.Buffer
+	skip := true
+	for _, char := range s {
+		if unicode.IsSpace(char) {
+			if !skip {
+				buffer.WriteRune(' ')
+				skip = true
+			}
+		} else {
+			buffer.WriteRune(char)
+			skip = false
+		}
+	}
+
+	s = buffer.String()
+
+	if skip && len(s) > 0 {
+		s = s[:len(s)-1]
+	}
+
+	return s
+}
+
+func TestSimplifyWitespace() {
+	s := "ssss   ssssss ss        sss ssssssss     "
+	s = SimplifyWitespace(s)
+	fmt.Println(s)
 }
--- src/str/str.go
+++ src/str/str.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
+	str5()
+}
+
+func str1() {
+	x, err := strconv.ParseFloat("-99.7", 64)
+	fmt.Printf("%8T %6v %v\n", x, x, err)
+
+	y, err := strconv.ParseInt("71309", 10, 0)
+	fmt.Printf("%8T %6v %v\n", y, y, err)
+
+	z, err := strconv.Atoi("71309")
+	fmt.Printf("%8T %6v %v\n", z, z, err)
+}
+
+func str2() {
 	text1 := "\"what's that?\", he said"
 	text2 := `"what's that?", he said`
 	text3 := "\u221A \U0000221a"
@@ -10,3 +28,40 @@ func main() {
 	fmt.Println(text2)
 	fmt.Println(text3)
 }
+
+func str3() {
+	z := 10000
+	s := strconv.FormatBool(z > 100)
+	fmt.Println(s)
+
+	i, err := strconv.ParseInt("0xDEED", 0, 32)
+	fmt.Println(i, err)
+
+	j, err := strconv.ParseInt("0707", 0, 32)
+	fmt.Println(j, err)
+
+	k, err := strconv.ParseInt("10111010001", 2, 32)
+	fmt.Println(k, err)
+}
+
+func str4() {
+	i := 16769023
+	fmt.Println(strconv.Itoa(i))
+	fmt.Println(strconv.FormatInt(int64(i), 10))
+	fmt.Println(strconv.FormatInt(int64(i), 2))
+	fmt.Println(strconv.FormatInt(int64(i), 16))
+}
+
+func str5() {
+	s := "Alice is g刘irl ,, hafas"
+	quoted := strconv.Quote(s)
+	fmt.Println(quoted)
+	fmt.Println(strconv.Unquote(quoted))
+}
+
+func str6() {
+}
+
+
+
+
